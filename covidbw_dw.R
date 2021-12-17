985b45ba857dc93636998930b93a070beb13e5bd
stefan <moogs@gmx.de>
2021-12-17 23:03:39 +0100

    Add script to update interactive DW map.

---
+++ covidbw_dw.R
@@ -0,0 +1,20 @@
+# Interactive Map ---------------------------------------------------------
+
+library(dwextra)
+library(DatawRappr)
+
+date <- as.Date("2021-12-12")
+
+covid_bw_dw <- read_csv(glue::glue("data-raw/csv/covidbw_{date}.csv"))  %>%
+  mutate(across(ends_with("q"), ~ 100 * .x, .names = "{.col}1"),
+         district = case_when(
+           district == "Heilbronn, Land" ~ "Heilbronn Landkreis",
+           district == "Heilbronn, Stadt" ~ "Heilbronn Stadt",
+           district == "Karlsruhe, Land" ~ "Karlsruhe Landkreis",
+           district == "Karlsruhe, Stadt" ~ "Karlsruhe Stadt",
+           TRUE ~ district
+         )) %>%
+  filter(!grepl("^Baden-W", district))
+
+dw_data_to_chart(covid_bw_dw, "7BugB")
+dw_edit_chart("7BugB", intro = format(date, "%B %d, %Y"))
