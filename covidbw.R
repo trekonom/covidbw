22ac7c3733d6fb28c44fd4bb4fa65cf8840c3b08
stefan <moogs@gmx.de>
2022-03-23 11:01:19 +0100

    Update data.

--- covidbw.R
+++ covidbw.R
@@ -4,7 +4,7 @@ require(ggtext)
 
 source("R/covid_charts.R", encoding = "UTF-8")
 
-date <- as.Date("2022-03-14")
+date <- as.Date("2022-03-21")
 
 # Theme -------------------------------------------------------------------
 base_family <- "Roboto Condensed"
